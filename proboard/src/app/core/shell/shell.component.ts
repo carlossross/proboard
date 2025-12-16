5acf7f60c8ae8b2724b3fbf6681433eefc3fb352
carlossross <carlos.rossv@gmail.com>
2025-12-16 16:35:11 -0600

    chore(core): Add Basic Shell App

---
+++ proboard/src/app/core/shell/shell.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'proboard-shell',
+  imports: [],
+  templateUrl: './shell.component.html',
+  styleUrl: './shell.component.scss',
+})
+export class ShellComponent {}
